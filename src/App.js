aa6f8b3c1910750a103afdc9f290f0a4cec65749
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2021-02-04 10:01:28 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>hello world React App</h1>
+     <h1>hello world React App BootCamp 2020</h1>
     </div>
   );
 }
