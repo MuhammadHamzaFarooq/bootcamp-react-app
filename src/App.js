a89129a24138c566fd9cf8142dbf0093fd355c7f
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2021-02-04 13:05:08 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import  './App.css';
+
 function App() {
-  return (
-    <div className="App">
-     <h1>hello world React App BootCamp 2020 class - 02</h1>
-    </div>
-  );
+    return ( 
+       < div className = "App" >
+            <h1>hello world</h1>
+      </div>
+    );
 }
 
 export default App;
